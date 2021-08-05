97b4ce1a4dbef3cc8cc48fc56a5712f66ae11ff1
temlerg <dima2600ru@bk.ru>
2021-08-05 16:25:06 +0300

    hive

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+import 'contact_page.dart';
+
+void main() async {
+  final appDocument = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocument.path);
+  runApp(MyApp());
+
+  final openBox = await Hive.openBox("contacts");
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hive Tutorial',
+      home: FutureBuilder(
+        future: Hive.openBox('contacts'),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError)
+              return Text(snapshot.error.toString());
+            else
+              return ContactPage();
+          } else
+            return Scaffold();
+        },
+      ),
+    );
+  }
+}
